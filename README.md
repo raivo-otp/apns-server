a68ff3b33aedf2946606acd68f01927c53829a2d
Tijme Gommers <t.gommers@outlook.com>
2021-08-21 11:42:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+<p align="center" style="color: red"><b>Raivo APNS is currently under development. Raivo APNS is not ready for production.</b></p>
+<hr><br>
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/raivo-otp/ios-application/master/Assets/app-icon.png" width="200"/>
+</p>
+<p align="center">
+    <a href="https://github.com/raivo-otp/apns-server/blob/master/LICENSE.md"><img src="https://raw.finnwea.com/shield/?firstText=Source&secondText=Licensed" /></a>
+    <a href="https://itunes.apple.com/"><img src="https://raw.finnwea.com/shield/?firstText=Platform&secondText=PHP" /></a>
+    <a href="https://github.com/raivo-otp/apns-server/releases"><img src="https://raw.finnwea.com/vector-shields-v1/?typeKey=SemverVersion&typeValue1=raivo-otp&typeValue2=apns-server&typeValue4=Release&cache=4"></a>
+    <br/>
+    <b>An Apple Push Notification Service (APNS) that proxies tokens from Raivo for MacOS to Raivo for iOS</b>
+    <br/>
+    <sup>Built by <a href="https://www.linkedin.com/in/tijme/">Tijme Gommers</a> – Buy me a coffee via <a href="https://www.paypal.me/tijmegommers">PayPal</a></sup>
+    <br/>
+</p>
+
+## Features
+
+* ToDo
+* It's open-source (check the [contribution policy](https://github.com/raivo-otp/apns-server/blob/master/CONTRIBUTING.md))
+
+## Issues
+
+Issues or new features can be reported via the [issue tracker](https://github.com/raivo-otp/apns-server/issues). Please make sure your issue or feature has not yet been reported by anyone else before submitting a new one.
+
+## Security
+
+If you discover a security vulnerability, we would like to know about it so we can take steps to address it as quickly as possible. Please refer to the [security policy](https://github.com/raivo-otp/apns-server/blob/master/SECURITY.md) for information on reporting vulnerabilities.
+
+## Development
+
+Run a local PHP development server with the `public` folder as root.
+
+    php -S localhost:1337 -t public
+
+## Privacy
+
+Raivo does not collect personally identifiable information in any way. Please refer to the [privacy policy](https://github.com/raivo-otp/apns-server/blob/master/PRIVACY.md) for information.
+
+## License
+
+Copyright (c) 2019 Tijme Gommers. All rights reserved. View [LICENSE.md](https://github.com/raivo-otp/apns-server/blob/master/LICENSE.md) for the full license.
---
+++ public/v1/apns.php
@@ -0,0 +1,162 @@
+<?php
+
+    /* Raivo OTP APNS
+     * 
+     * Workflow:
+     *      1. iPhone: Scans MacOS receiver QR-code (containing MacOS push token & MacOS encryption key)
+     *      2. iPhone: Sends MacOS receiver a type 1 (copy to clipboard) notification containing a password, encrypted with the encryption key
+     */
+
+    // Define settings
+    define('APNS_PRODUCTION', false);
+    define('APNS_DEVELOPMENT', !APNS_PRODUCTION);
+    define('APNS_AUTHKEYPATH', realpath(dirname(__FILE__) . '/../../apns_key.p8'));
+    define('APNS_AUTHKEY', openssl_pkey_get_private('file://' . APNS_AUTHKEYPATH));
+    define('APNS_KEYID', '');
+    define('APNS_TEAMID', '');
+    define('APNS_BUNDLEID', 'me.tij.Raivo-MacOS');
+    define('APNS_URI', APNS_PRODUCTION ? 'https://api.push.apple.com' : 'https://api.development.push.apple.com');
+
+    // Show PHP errors in development runs
+    if (APNS_DEVELOPMENT) {
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+    }
+
+    // All output data should be JSON
+    header('Content-Type: application/json');
+
+    // Helper function for I/O
+    function respondWith(string $message, bool $success = true) {
+        exit(json_encode(array(
+            'status' => $success ? 'success' : 'error',
+            'message' => $message
+        )));
+    }
+
+    // Helper function to retrieve user input (string)
+    function getUserInputString(string $key): ?string {
+        if (isset($_POST[$key]) && is_string($_POST[$key])) {
+            return $_POST[$key];
+        }
+
+        return null;
+    }
+
+    // Helper function to retrieve user input (int)
+    function getUserInputInt(string $key): ?int {
+        if (isset($_POST[$key]) && is_numeric($_POST[$key])) {
+            return $_POST[$key];
+        }
+
+        return null;
+    }
+
+    // Helper function to retrieve user input (array of strings)
+    function getUserInputStringArray(string $key, ?int $requiredStringLength = null): ?array {
+        if (isset($_POST[$key]) && is_array($_POST[$key])) {
+            foreach($_POST[$key] as $value) {
+                if (!is_string($value)) {
+                    return null;
+                }
+
+                if (!is_null($requiredStringLength) && strlen($value) != $requiredStringLength) {
+                    return null;
+                }
+            }
+
+            return $_POST[$key];
+        }
+
+        return null;
+    }
+
+    // Helper function to base64 encode replacing +/ with -_ and trimming = signs
+    function base64(array $data): string {
+        return rtrim(strtr(base64_encode(json_encode($data)), '+/', '-_'), '=');
+    }
+
+    // Validate authentication key
+    if (APNS_AUTHKEY === false) {
+        respondWith('Fatal misconfiguration. Could not find the authentication key on the server.');
+    }
+
+    // Retrieve all user input
+    $deviceTokens = getUserInputStringArray('deviceTokens', 64);
+    $notificationType = getUserInputInt('notificationType');
+    $notificationToken = getUserInputString('notificationToken');
+    $notificationIssuer = getUserInputString('notificationIssuer');
+    $notificationAccount = getUserInputString('notificationAccount');
+
+    // Validate device tokens
+    if (empty($deviceTokens)) {
+        respondWith('Incorrect device token array.', false);
+    }
+
+    if (count($deviceTokens) > 10) {
+        respondWith('Maximum amount of device tokens reached.', false);
+    }
+
+    // Build JWT header & claims
+    $header = base64([
+        'alg' => 'ES256',
+        'kid' => APNS_KEYID
+    ]);
+
+    $claims = base64([
+        'iss' => APNS_TEAMID,
+        'iat' => time()
+    ]);
+
+    // Sign JWT header & claims
+    $signature = '';
+    openssl_sign($header . '.' . $claims, $signature, APNS_AUTHKEY, 'sha256');
+    $jwt = $header . '.' . $claims . '.' . base64_encode($signature);
+
+    // Build CURL handle/request
+    $handle = curl_init();
+
+    // Send push notifications
+    foreach($deviceTokens as $deviceToken) {
+        curl_setopt_array($handle, [
+            CURLOPT_URL => APNS_URI. '/3/device/' . $deviceToken,
+            CURLOPT_PORT => 443,
+            CURLOPT_HTTPHEADER => [
+                'apns-push-type: ' . 'background',
+                'apns-topic: ' . APNS_BUNDLEID,
+                'Authorization: Bearer ' . $jwt,
+                'User-Agent: Raivo OTP for MacOS APNS server'
+            ],
+            CURLOPT_POST => TRUE,
+            CURLOPT_POSTFIELDS => json_encode([
+                'type' => $notificationType,
+                'token' => $notificationToken,
+                'issuer' => $notificationIssuer,
+                'account' => $notificationAccount
+            ]),
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+            CURLOPT_TIMEOUT => 5,
+            CURLOPT_HEADER => 1
+        ]);
+
+        $curl_status = curl_exec($handle);
+
+        if ($curl_status === false) {
+            respondWith('Stopped sending push notifications as a curl error occurred: ' . curl_error($handle), false);
+            break;
+        }
+
+        $http_status = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+
+        if ($http_status !== 200) {
+            respondWith('Stopped sending push notifications as APS responded with an error: ' . curl_error($handle), false);
+            break;
+        }
+    }
+
+    // Close handle
+    curl_close($handle);
+
+    // Exit with a success message
+    respondWith('Token delivered at ' . count($deviceTokens) . ' device(s).');
